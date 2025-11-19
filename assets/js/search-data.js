6a2dfe8427230648684e72fac2f4236783cc8939
kooshatab <166946612+kooshatab@users.noreply.github.com>
2025-11-19 20:15:00 +0000

    Deploying to gh-pages from @ kooshatab/kooshatab.github.io@7b8feb0b6b918ae02eefaf2cb271e7f69b5f6945 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,8 +451,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-attrition-data-analysis-amp-modeling",
+          title: 'Attrition Data Analysis &amp;amp; Modeling',
           description: "Frito Lay Attrition Data Analysis",
           section: "Projects",handler: () => {
               window.location.href = "/projects/FritoLay/";
